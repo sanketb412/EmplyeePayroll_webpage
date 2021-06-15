8e4d6f684638b18dae462b3f90e879ab7f54c6a2
sanketb412 <sanketbagde38@gmail.com>
2021-06-15 17:45:07 +0530

    Added: code for EmployeePayroll to get details from Local Storage

--- js/new_home.js
+++ js/new_home.js
@@ -1,12 +1,21 @@
+let empPayrollList;
 window.addEventListener('DOMContentLoaded', (event) => {
+    empPayrollList = getEmployeePayrollDataFromStorage();
+    document.querySelector(".emp-count").textContent = empPayrollList.length;
     createInnerHtml();
+    localStorage.removeItem('editEmp');
 });
+
+const getEmployeePayrollDataFromStorage = () => {
+    return localStorage.getItem('EmployeePayrollList') ?
+                        JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+}
 
 const createInnerHtml = () => {
     const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
                         "<th>Salary</th><th>Start Date</th><th>Actions</th>";
-    let innerHtml = `${headerHtml}`
-    let empPayrollList = creteEmployeePayrollJSON();
+    if (empPayrollList.length == 0) return;
+    let innerHtml = `${headerHtml}`;
     for(const empPayrollData of empPayrollList) {
         innerHtml = `${innerHtml}
         <tr>
