651209b31ef079dfa50e9214d975d09832fe6ad3
sanketb412 <sanketbagde38@gmail.com>
2021-06-17 15:38:01 +0530

    Added: code for remove and edit icon function

--- js/new_payroll_form.js
+++ js/new_payroll_form.js
@@ -1,4 +1,3 @@
-
 window.addEventListener('DOMContentLoaded', (event) => {
     const name=document.querySelector('#name');
     const textError = document.querySelector('.text-error');
@@ -8,43 +7,34 @@ window.addEventListener('DOMContentLoaded', (event) => {
             return;
         }
         try {
-            (new EmployeePayrollData())._name = name.value;
+            (new EmployeePayrollData()).name = name.value;
             textError.textContent="";
         }catch (e) {
             textError.textContent=e;
-        }
-    });
+        } 
+    });   
 
-    const date = document.querySelector('#date');
-    const dateError = document.querySelector('.date-error');
-    date.addEventListener('input', function () {
-        let startDate = getInputValueById('#day')+" "+
-                          getInputValueById('#month')+" "+
-                          getInputValueById('#year');
-        try {
-             checkStartDate(new Date(Date.parse(startDate)));
-           // (new EmployeePayrollData()).startDate = new date(Date.parse(startDate));
-            dateError.textContent=" ";
-        }catch (e) {
-            dateError.textContent=e;
-        }
-    });
-
-   
     const salary = document.querySelector('#salary');
     const output = document.querySelector('.salary-output');
     output.textContent = salary.value;
     salary.addEventListener('input', function() {
-        output.textContent = salary.value; 
+    output.textContent = salary.value; 
     }); 
 
-    // checkForUpdate();
+    const date = document.querySelector('#date');
+    const dateError = document.querySelector('.date-error');
+    date.addEventListener('input', function () {
+        const startDate = getInputValueById('#day')+" "+
+                          getInputValueById('#month')+" "+
+                          getInputValueById('#year');
+        try {
+            checkStartDate(new Date(Date.parse(startDate)));
+            dateError.textContent="";
+        }catch (e) {
+            dateError.textContent=e;
+        }
+    });
 });
-
-const checkStartDate = (startDate) => {
-    let now = new Date();
-    if (startDate > now) throw 'Start Date is Future Date!';
-}
 
 const save = () => {
     try{
@@ -57,11 +47,10 @@ const save = () => {
 
 const createEmployeePayroll = () => {
     let employeePayrollData = new EmployeePayrollData();
-    // employeePayrollData._id = new Date().getTime();
-    try{
+    try {
         employeePayrollData.name=getInputValueById('#name');
-    }catch(e){
-        setTextValue('.text-error',e);
+    } catch(e) {
+        setTextValue('.text-error', e);
         throw e;
     }
     employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
@@ -70,9 +59,47 @@ const createEmployeePayroll = () => {
     employeePayrollData.salary = getInputValueById('#salary');
     employeePayrollData.note = getInputValueById('#notes');
     let date = getInputValueById('#day') +" "+getInputValueById('#month')+" "+getInputValueById('#year');
-    employeePayrollData.startDate = Date.parse(date);
+    employeePayrollData.date = Date.parse(date);
     alert(employeePayrollData.toString());
     return employeePayrollData;
+}
+
+const  getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let selItems= [];
+    allItems.forEach(item => {
+        if(item.checked) selItems.push(item.value);
+    });
+    return selItems;
+}
+
+/**
+ * 1: querrySelector is the newer feature.
+ * 2: The querySelector method can be used when selecting by element name,
+ *    nesting, or class name.
+ * 3: quereySelector lets you find elements with rules that cant be
+ *    expressed with getElementByID.
+ */
+const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+}
+
+/**
+ * 1: getElemtByID is better supported than quereySelector in older versions
+ *    of the Browser
+ * 2: The thing with getElementByID is that, it only allows to select an
+ *    element by its ID
+ */
+
+const getInputElementValue = (id) => {
+    let value = document.getElementById(id).value;
+    return value;
+}
+
+const checkStartDate = (startDate) => {
+    let now = new Date();
+    if (startDate > now) throw 'Start Date is Future Date!';
 }
 
 function createAndUpdateStorage(employeePayrollData){
@@ -86,51 +113,6 @@ function createAndUpdateStorage(employeePayrollData){
     alert(employeePayrollList.toString());
     localStorage.setItem("EmployeePayrollList",JSON.stringify(employeePayrollList))
 }
-
-// const  getSelectedValues = (propertyValue) => {
-//     let allItems = document.querySelectorAll(propertyValue);
-//     let selItems= [];
-//     allItems.forEach(item => {
-//         if(item.checked) selItems.push(item.value);
-//     });
-//     return selItems;
-// }
-
-const getInputValueById = (id) => {
-    let value = document.querySelector(id).value;
-    return value;
-}
-
-// *
-//  * 1: getElementById is better supported than querySelector in older versions of the browsers.
-//  * 2: The thing with getElementById is that iot only allows to select an element by its id.
-//  *
-
-// const getInputElementValue = (id) => {
-//     let value = document.getElementById(id).value;
-//     return value;
-// }
-
-const  getSelectedValues = (propertyValue) => {
-    let allItems = document.querySelectorAll(propertyValue);
-    let selItems= [];
-    allItems.forEach(item => {
-        if(item.checked) selItems.push(item.value);
-    });
-    return selItems;
-}
-
-// function createAndUpdateStorage(employeePayrollData){
-//     let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
-
-//     if(employeePayrollList != undefined){
-//         employeePayrollList.push(employeePayrollData);
-//     }else{
-//         employeePayrollList=[employeePayrollData]
-//     }
-//     alert(employeePayrollList.toString());
-//     localStorage.setItem("EmployeePayrollList",JSON.stringify(employeePayrollList))
-// }
 
 const resetForm = () => {
     setValue("#name", '');
