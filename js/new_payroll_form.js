ca486072c4bb79f200a758037185c589c4d214a0
sanketb412 <sanketbagde38@gmail.com>
2021-06-17 19:03:24 +0530

    Refactor: code

--- js/new_payroll_form.js
+++ js/new_payroll_form.js
@@ -1,162 +1,139 @@
-
 window.addEventListener('DOMContentLoaded', (event) => {
-    const name=document.querySelector('#name');
+    const name = document.querySelector("#name");
     const textError = document.querySelector('.text-error');
-    name.addEventListener('input', function() {
+    name.addEventListener("input", function() {
         if(name.value.length == 0) {
-            textError.textContent="";
+            textError.textContent = " ";
             return;
         }
         try {
-            (new EmployeePayrollData())._name = name.value;
-            textError.textContent="";
-        }catch (e) {
-            textError.textContent=e;
+            (new EmployeePayrollData()).name = name.value;
+            textError.textContent = "";
+        } catch(e) {
+            textError.textContent = e;
         }
     });
 
     const date = document.querySelector('#date');
-    const dateError = document.querySelector('.date-error');
-    date.addEventListener('input', function () {
-        let startDate = getInputValueById('#day')+" "+
-                          getInputValueById('#month')+" "+
-                          getInputValueById('#year');
+    const dateError = document.querySelector(".date-error");
+    date.addEventListener('input', function() {
+        let startDate = getInputValueById('#day')+ " " +getInputValueById('#month')+ " " +getInputValueById('#year');
         try {
-             checkStartDate(new Date(Date.parse(startDate)));
-           // (new EmployeePayrollData()).startDate = new date(Date.parse(startDate));
-            dateError.textContent=" ";
-        }catch (e) {
-            dateError.textContent=e;
+            checkStartDate(new Date(Date.parse(startDate)));
+            // (new EmployeePayrollData()).startDate = new date(Date.parse(startDate));
+            dateError.textContent= " ";
+        } catch (e) {
+            dateError.textContent = e;
         }
     });
 
-   
-    const salary = document.querySelector('#salary');
-    const output = document.querySelector('.salary-output');
+    const salary = document.querySelector("#salary");
+    const output = document.querySelector(".salary-output");
     output.textContent = salary.value;
-    salary.addEventListener('input', function() {
-        output.textContent = salary.value; 
-    }); 
-
-    // checkForUpdate();
+    salary.addEventListener("input", function() {
+        output.textContent = salary.value;
+    });
 });
 
 const checkStartDate = (startDate) => {
     let now = new Date();
-    if (startDate > now) throw 'Start Date is Future Date!';
+    if(startDate > now) {
+        throw "StartDate is Future Date!";
+    }
 }
-
 const save = () => {
-    try{
+    try {
         let employeePayrollData = createEmployeePayroll();
         createAndUpdateStorage(employeePayrollData);
-    }catch(e){
+    } catch(e) {
         return;
     }
 }
 
 const createEmployeePayroll = () => {
     let employeePayrollData = new EmployeePayrollData();
-    // employeePayrollData._id = new Date().getTime();
-    try{
-        employeePayrollData.name=getInputValueById('#name');
-    }catch(e){
-        setTextValue('.text-error',e);
+    try {
+        employeePayrollData.name = getInputValueById("#name");
+    } catch(e) {
+        setTextValue(".text-error",e);
         throw e;
     }
-    employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
-    employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
-    employeePayrollData.department = getSelectedValues('[name=department]');
-    employeePayrollData.salary = getInputValueById('#salary');
-    employeePayrollData.note = getInputValueById('#notes');
-    let date = getInputValueById('#day') +" "+getInputValueById('#month')+" "+getInputValueById('#year');
+    employeePayrollData.profilePic = getSelectedValues("[name=profile]").pop();
+    employeePayrollData.gender = getSelectedValues("[name=gender]").pop();
+    employeePayrollData.department = getSelectedValues("[name=department]");
+    employeePayrollData.salary = getInputValueById("#salary");
+    employeePayrollData.note = getInputValueById("#notes");
+    let date = getInputValueById("#day")+ " " + getInputValueById("#month") +" "+ getInputValueById("#year");
     employeePayrollData.date = Date.parse(date);
     alert(employeePayrollData.toString());
     return employeePayrollData;
 }
 
-function createAndUpdateStorage(employeePayrollData){
+function createAndUpdateStorage(employeePayrollData) {
     let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
-
-    if(employeePayrollList != undefined){
+    if (employeePayrollList != undefined) {
         employeePayrollList.push(employeePayrollData);
-    }else{
-        employeePayrollList=[employeePayrollData]
+    } else {
+        employeePayrollList = [employeePayrollData];
     }
     alert(employeePayrollList.toString());
-    localStorage.setItem("EmployeePayrollList",JSON.stringify(employeePayrollList))
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
 }
 
-// const  getSelectedValues = (propertyValue) => {
-//     let allItems = document.querySelectorAll(propertyValue);
-//     let selItems= [];
-//     allItems.forEach(item => {
-//         if(item.checked) selItems.push(item.value);
-//     });
-//     return selItems;
-// }
-
+/*
+ * 1: querySelector is the newer feature.
+ * 2: The querySelector method can be used when selecting by element name, nesting, or class name.
+ * 3: querySelector lets you foind elements with rules that can't be expressed with getElementById
+ */
 const getInputValueById = (id) => {
     let value = document.querySelector(id).value;
     return value;
 }
 
-// *
-//  * 1: getElementById is better supported than querySelector in older versions of the browsers.
-//  * 2: The thing with getElementById is that iot only allows to select an element by its id.
-//  *
+/*
+ * 1: getElementById is better supported than querySelector in older versions of the browsers.
+ * 2: The thing with getElementById is that iot only allows to select an element by its id.
+ *
+const getInputValueById = (id) => {
+    let value = document.getElementById(id).value;
+    return value;
+}
+*/
 
-// const getInputElementValue = (id) => {
-//     let value = document.getElementById(id).value;
-//     return value;
-// }
-
-const  getSelectedValues = (propertyValue) => {
-    let allItems = document.querySelectorAll(propertyValue);
-    let selItems= [];
+const getSelectedValues = (propertyValue) => {
+    let allItems =document.querySelectorAll(propertyValue);
+    let selItems = [];
     allItems.forEach(item => {
         if(item.checked) selItems.push(item.value);
     });
     return selItems;
 }
 
-// function createAndUpdateStorage(employeePayrollData){
-//     let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
-
-//     if(employeePayrollList != undefined){
-//         employeePayrollList.push(employeePayrollData);
-//     }else{
-//         employeePayrollList=[employeePayrollData]
-//     }
-//     alert(employeePayrollList.toString());
-//     localStorage.setItem("EmployeePayrollList",JSON.stringify(employeePayrollList))
-// }
-
 const resetForm = () => {
-    setValue("#name", '');
+    setValue('#name','');
     unsetSelectedValues('[name=profile]');
     unsetSelectedValues('[name=gender]');
     unsetSelectedValues('[name=department]');
     setValue('#salary','');
     setValue('#notes','');
-    setSelectedIndex('#day',0);
-    setSelectedIndex('#month', 0);
-    setSelectedIndex('#year',0);
+    setValue('#day','1');
+    setValue('#month','January');
+    setvalue('#year','2021');
 }
 
 const unsetSelectedValues = (propertyValue) => {
     let allItems = document.querySelectorAll(propertyValue);
     allItems.forEach(item => {
         item.checked = false;
-    })
+    }); 
 }
 
-const setTextValue = (id, value) => {
+const setTextValue = () => {
     const element = document.querySelector(id);
     element.textContent = value;
 }
 
-const setValue = (id, value ) => {
+const setValue = (id, value) => {
     const element = document.querySelector(id);
     element.value = value;
 }
